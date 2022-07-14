826249a4892af6202275c8c563cc3babae4f3a3b
Philip Levy <philipdlevy@gmail.com>
2022-07-14 10:40:16 -0600

    commit tree 9c52101ae41e2acd45c12b72a7022494ad66d3f4

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,46 @@
 class Bird < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    # added rescue_from
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+
+    # def create
+    #     bird = Bird.create(bird_params)
+    #     if bird.valid?
+    #       render json: bird, status: :created
+    #     else
+    #       render json: { errors: bird.errors }, status: :unprocessable_entity
+    #     end
+    # end
+
+    # cleaner way of doing same thing
+    # def create
+    #     bird = Bird.create!(bird_params)
+    #     render json: bird, status: :created
+    #   rescue ActiveRecord::RecordInvalid => invalid
+    #     render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    # end
+
+      #using rescue_from
+      def create
+        # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+        bird = Bird.create!(bird_params)
+        render json: bird, status: :created
+      end
+
+      def update
+        bird = find_bird
+        # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+        bird.update!(bird_params)
+        render json: bird
+      end
+
+
+      private
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
 end
